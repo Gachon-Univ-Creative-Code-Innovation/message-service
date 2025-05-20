36245992bbb35712ba9d155ac810ffc07b7b2aae
jeonjunbae <wnsqowid0901@gmail.com>
2025-05-20 12:03:08 +0900

    chore: CORS 설정

--- src/main/java/com/gucci/message_service/config/WebMvcConfig.java
+++ src/main/java/com/gucci/message_service/config/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOriginPatterns("http://127.0.0.1:5500", "http://localhost:5173","https://a-log.netlify.app","http://43.201.107.237:5173") // 테스트용 포트
+                .allowedOriginPatterns("http://127.0.0.1:5500", "http://localhost:5173","https://a-log.netlify.app","http://a-log.site","https://a-log.site") // 테스트용 포트
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("Cache-Control", "Content-Language", "Content-Type", "Expires", "Last-Modified", "Pragma")
